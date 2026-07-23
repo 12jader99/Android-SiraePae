10e699222c8e3b1760afbf3c6ce7629f3d8cb4a4
Yeimi Alejandra Pipicano Gomez <alemgoz12@gmail.com>
2026-07-23 03:24:31 -0500

    Algunos cambios

--- app/src/main/java/com/example/siraeandroid/MainActivity.kt
+++ app/src/main/java/com/example/siraeandroid/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.siraeandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.siraeandroid.databinding.ActivityMainBinding
+import com.example.siraeandroid.databinding.ActivityRegistroBinding
+import kotlin.toString
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnRegistrar.setOnClickListener {
+
+            val nombre = binding.etNombre.text.toString().trim()
+            val cedula = binding.etCedula.text.toString().trim()
+            val correo = binding.etCorreo.text.toString().trim()
+            val password = binding.etContrasena.text.toString().trim()
+
+            if (nombre.isEmpty() || cedula.isEmpty() || correo.isEmpty() || password.isEmpty()) {
+
+                Toast.makeText(this, "Que complete todos los campos", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show()
+
+            }
         }
+
+
+        binding.txtVolver.setOnClickListener {
+
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+
+        }
+
+
+
     }
+
+
+
 }
+
+
--- app/src/main/java/com/example/siraeandroid/Registro.kt
+++ app/src/main/java/com/example/siraeandroid/Registro.kt
@@ -12,7 +12,7 @@ import com.example.siraeandroid.databinding.ActivityRegistroBinding
 
 class Registro : AppCompatActivity() {
 
-    private lateinit var  binding: ActivityRegistroBinding
+    private lateinit var binding: ActivityRegistroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,39 +20,11 @@ class Registro : AppCompatActivity() {
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnRegistrar.setOnClickListener {
-
-            val nombre = binding.etNombre.text.toString().trim()
-            val cedula = binding.etCedula.text.toString().trim()
-            val correo = binding.etCorreo.text.toString().trim()
-            val password = binding.etContrasena.text.toString().trim()
-
-            if (nombre.isEmpty() || cedula.isEmpty() || correo.isEmpty() || password.isEmpty()) {
-
-                Toast.makeText(this, "Que complete todos los campos", Toast.LENGTH_SHORT).show()
-
-            } else {
-
-                    Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show()
-
-            }
-        }
-
-
-        binding.txtVolver.setOnClickListener {
-
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-
-        }
-
-
-
     }
 
-
-
 }
+
+
 
 
 
